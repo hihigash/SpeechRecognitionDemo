a99ab694d320f25acb42c8d1f7cc161e392bf448
Hirokazu Higashino <hihigash@live.jp>
2017-03-31 15:13:19 +0900

    Delete NAudio

--- SpeechRecognitionDemo/Program.cs
+++ SpeechRecognitionDemo/Program.cs
@@ -7,7 +7,6 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.CognitiveServices.SpeechRecognition;
-using NAudio.Wave;
 
 namespace SpeechRecognitionDemo
 {
